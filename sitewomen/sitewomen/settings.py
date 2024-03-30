acd49848502f3c24f75ef19c8c41b589166ed770
Mikhail <147713243+ozdorova@users.noreply.github.com>
2024-03-30 13:51:08 +0300

    урок 11.2 подключение PostgreSQL

--- sitewomen/sitewomen/settings.py
+++ sitewomen/sitewomen/settings.py
@@ -124,13 +124,24 @@ WSGI_APPLICATION = 'sitewomen.wsgi.application'
 # python manage.py loaddata db.json
 
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
+#подключение PostgreSQL
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': "django.db.backends.postgresql",
+        'NAME': 'sitewomen_db',
+        'USER': 'sitewomen',
+        'PASSWORD': '1234',
+        'HOST': 'localhost',
+        'PORT': 5432,
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
